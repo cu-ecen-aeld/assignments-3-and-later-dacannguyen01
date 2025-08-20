c4cbc8a970f4203796ea0640797622f25ef11ae2
Dac An Nguyen <nguyendacan2001@gmail.com>
2025-08-20 23:50:27 +0700

    assignment 7 part 1 completed

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,9 +29,34 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
+	
     /**
     * TODO: implement per description
     */
+    if(buffer == NULL)
+    return NULL;
+
+    size_t char_count = (buffer->entry[buffer->out_offs]).size;
+    int buffer_count = buffer->out_offs;
+    size_t last_pos = 0;
+
+    while(char_offset > (char_count - 1))
+    {
+        last_pos = char_count;
+        buffer_count = (buffer_count + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+        
+        if(buffer_count == buffer->out_offs)
+        return NULL;
+
+        char_count += (buffer->entry[buffer_count]).size;
+    }
+
+    if(char_offset <= (char_count - 1))
+    {
+        *entry_offset_byte_rtn = char_offset - last_pos;
+        return &buffer->entry[buffer_count];
+    }
+    
     return NULL;
 }
 
@@ -47,6 +72,22 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+    if(buffer == NULL || add_entry == NULL || add_entry->buffptr == NULL || add_entry->size == 0)
+    return;
+
+    buffer->entry[buffer->in_offs] = *add_entry;
+
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    if(buffer->full)
+    {
+        buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    if(buffer->in_offs == buffer->out_offs)
+    {
+        buffer->full = 1;
+    }
 }
 
 /**
